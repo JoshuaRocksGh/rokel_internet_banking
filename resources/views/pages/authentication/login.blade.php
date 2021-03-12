8c63eba2d391839cd70d8a33e9b2de9975448b65
king kobby <ampahkwabena5@gmail.com>
2021-03-12 11:18:09 +0000

    almost done with the own account page

--- resources/views/pages/authentication/login.blade.php
+++ resources/views/pages/authentication/login.blade.php
@@ -51,12 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group mb-3">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="checkbox-signin">
-                                <label class="custom-control-label" for="checkbox-signin">Remember me</label>
-                            </div>
-                        </div>
+
                         <div class="form-group mb-0 text-center">
                             <a href="{{ url('home') }}">
                                 <button class="btn btn-primary btn-block" type="button">Log In </button>
@@ -83,18 +78,21 @@
             <div class="auth-user-testimonial">
                 <div class="row">
                     <div class="col-lg-2">
-
+                        <div class="avatar-lg rounded-circle bg-soft-info border-info border">
+                            <i class="fe-heart font-22 avatar-title text-info"></i>
+                        </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="text-center card-box">
                             <div class="pt-2 pb-2">
                                 {{-- <img src="../assets/images/users/user-3.jpg" class="rounded-circle img-thumbnail avatar-xl" alt="profile-image"> --}}
-                                <i data-feather="credit-card" class="icon-dual font-22"></i>
+
+
+                                <i class="fas fa-book-open text-primary font-22" style="font-size: 100px;"></i>
                                 <h4 class="mt-3 font-16"><a href="extras-profile.html" class="text-dark">Open An Account</a>
                                 </h4>
 
-                                <button type="button" class="btn btn-primary btn-sm waves-effect waves-light">Click
-                                    Here</button>
+                                <button type="button" class="btn btn-primary btn-sm waves-effect waves-light">Here</button>
 
                             </div> <!-- end .padding -->
                         </div> <!-- end card-box-->
@@ -103,9 +101,7 @@
                     <div class="col-lg-4">
                         <div class="text-center card-box">
                             <div class="pt-2 pb-2">
-                                <img src="../assets/images/users/user-3.jpg" class="rounded-circle img-thumbnail avatar-xl"
-                                    alt="profile-image">
-
+                                <i class=" fas fa-desktop text-primary font-22" style="font-size: 100px;"></i>
                                 <h4 class="mt-3 font-16"><a href="extras-profile.html" class="text-dark">Make Enquiries</a>
                                 </h4>
                                 <button type="button" class="btn btn-primary btn-sm waves-effect waves-light">Click
@@ -126,8 +122,8 @@
                     <div class="col-lg-4">
                         <div class="text-center card-box">
                             <div class="pt-2 pb-2">
-                                <img src="../assets/images/users/user-3.jpg" class="rounded-circle img-thumbnail avatar-xl"
-                                    alt="profile-image">
+
+                                <i class="fas fa-headset text-primary font-22" style="font-size: 100px;"></i>
 
                                 <h4 class="mt-3 font-16"><a href="extras-profile.html" class="text-dark">FAQ</a></h4>
 
@@ -141,8 +137,8 @@
                     <div class="col-lg-4">
                         <div class="text-center card-box">
                             <div class="pt-2 pb-2">
-                                <img src="../assets/images/users/user-3.jpg" class="rounded-circle img-thumbnail avatar-xl"
-                                    alt="profile-image">
+
+                                <i class=" fas fa-map-marked-alt text-primary font-22" style="font-size: 100px;"></i>
 
                                 <h4 class="mt-3 font-16"><a href="extras-profile.html" class="text-dark">Branches</a></h4>
                                 <button type="button" class="btn btn-primary btn-sm waves-effect waves-light">Click
