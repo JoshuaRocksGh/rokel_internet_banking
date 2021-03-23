de756dee9d86dc39d7eaacf73dba1dc1bbddda11
USG <joshuaamarfio1@gmail.com>
2021-03-23 12:48:45 +0000

    changes made to the tabs of the portal

--- resources/views/snippets/side-bar.blade.php
+++ resources/views/snippets/side-bar.blade.php
@@ -176,18 +176,37 @@
                 </li>
 
                 <li>
-                    <a href="#sidebarEmail" data-toggle="collapse">
+                    <a href="#sidebarAccountServices" data-toggle="collapse">
                         <i class="mdi mdi-email-multiple-outline"></i>
                         <span> Account Services </span>
                         <span class="menu-arrow"></span>
                     </a>
-                    <div class="collapse" id="sidebarEmail">
+                    <div class="collapse" id="sidebarAccountServices">
                         <ul class="nav-second-level">
                             <li>
                                 <a href="#">Cheque Book Request </a>
                             </li>
                             <li>
                                 <a href="#">Activate Cheque Book</a>
+                            </li>
+                            <li>
+                                <a href="#sidebarChequeApprovals" data-toggle="collapse">
+                                    <span>Cheque Approvals</span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <div class="collapse" id="sidebarChequeApprovals">
+                                    <ul class="nav-second-level">
+                                        <li>
+                                            <a href="#">Pending</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Approved</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Rejected</a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </li>
                             <li>
                                 <a href="#">Stop Cheque</a>
