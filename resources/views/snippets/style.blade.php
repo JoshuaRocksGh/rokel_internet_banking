6ac7f5f71ddc06ffc428f592d7d6b5000b950149
Joshua Tetteh <josh.tetteh01@gmail.com>
2021-05-11 11:19:21 +0000

    rtgs styles

--- resources/views/snippets/style.blade.php
+++ resources/views/snippets/style.blade.php
@@ -105,8 +105,8 @@
             left: -14px;
             padding: 0.5rem;
             width: 20rem;
-            background:#ccc5b1;
-            color: #00008B;
+            background:#a19472;
+            color: #fff;
             text-align: center;
             font-family: 'Roboto', sans-serif;
             box-shadow: 4px 4px 15px rgba(26, 35, 126, 0.2);
