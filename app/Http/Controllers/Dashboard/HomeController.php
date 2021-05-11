9d5e2e246127b048b096ec8fbef4ec9777e28423
USG <joshuaamarfio1@gmail.com>
2021-05-11 12:25:31 +0000

    updates

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -39,7 +39,44 @@ class HomeController extends Controller
         // return $response->status();
         $result = new ApiBaseResponse();
         return $result->api_response($response);
-        
+
+    }
+
+    public function get_expenses(){
+        $authToken = session()->get('userToken');
+        $userID = session()->get('userId');
+
+        $base_response = new BaseResponse();
+
+        $data = [
+            "authToken" => $authToken,
+            "userId"    => $userID
+        ];
+        // return $data;
+        // return env('API_BASE_URL') ."account/getAccounts";
+
+        // $response = Http::post(env('API_BASE_URL') ."account/getAccounts", $data);
+
+
+        // return $response;
+        // return $response->status();
+        // $result = new ApiBaseResponse();
+        // return $result->api_response($response);
+        return $data;die;
+        return [
+            'responseCode' => '000',
+            'message' => 'spending analysis',
+            'data' => [
+            'travel' => '200',
+            'vendor payment' => '100',
+            'petty cash' => '300',
+            'salary' => '900',
+            'groceries' => '50',
+            'medical'=>'50',
+            'insurance'=>'950',
+            'tax'=>'95',
+            'others'=>'40']
+        ];
     }
 
 
