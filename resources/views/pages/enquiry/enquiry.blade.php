345f2a1b21a53e86ffb9abb397b67311144674c0
kwabena <ampah.kwabena@unionsg.com>
2021-05-14 10:55:43 +0000

    changes to ach

--- resources/views/pages/enquiry/enquiry.blade.php
+++ resources/views/pages/enquiry/enquiry.blade.php
@@ -2,54 +2,54 @@
 
 @section('content')
 
-@include('snippets.top_navbar', ['page_title' => 'Enquiry'])
+    @include('snippets.top_navbar', ['page_title' => 'Enquiry'])
 
 
 
-<div class="container">
+    <div class="container">
 
-    <br> <br> <br>
-  
+        <br> <br> <br>
+
         <br>
-    <div class=" card card-body">
-      
-        <div class="row ">
-           
-        <div class="col-md-2"></div>
+        <div class=" card card-body">
 
-            <div class="col-md-8">
+            <div class="row ">
 
-        
-              
+                <div class="col-md-2"></div>
 
-                <div class="row">
-                    <div class="col-md-2">  </div>
+                <div class="col-md-8">
 
-                    <div class="col-md-8">
-                        <div class="">
-                          
 
-                            <ul class="nav nav-tabs nav-bordered nav-justified">
-                                <li class="nav-item">
-                                    <a href="#home-b2" data-toggle="tab" aria-expanded="false" class="nav-link active">
-                                        Enquiry
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#profile-b2" data-toggle="tab" aria-expanded="true" class="nav-link ">
-                                        Complaint
-                                    </a>
-                                </li>
-                               
-                            </ul>
-                            <div class="tab-content">
-                                <div class="tab-pane active" id="home-b2">
-                                    <p>
+
+
+                    <div class="row">
+                        <div class="col-md-2"> </div>
+
+                        <div class="col-md-8">
+                            <div class="">
+
+
+                                <ul class="nav nav-tabs nav-bordered nav-justified">
+                                    <li class="nav-item">
+                                        <a href="#home-b2" data-toggle="tab" aria-expanded="false" class="nav-link active">
+                                            Enquiry
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#profile-b2" data-toggle="tab" aria-expanded="true" class="nav-link ">
+                                            Complaint
+                                        </a>
+                                    </li>
+
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane active" id="home-b2">
+                                        <p>
                                         <h4 class="text-primary text-center">Send us an enquiry</h4>
                                         <hr>
-                                    </p>
+                                        </p>
 
-                                    <p>
+                                        <p>
                                         <div class="">
                                             <form autocomplete="off" aria-autocomplete="off">
 
@@ -65,45 +65,47 @@
 
                                                 <div class="form-group">
                                                     <label for="example-textarea">Message</label>
-                                                    <textarea class="form-control" id="example-textarea" rows="5"></textarea>
+                                                    <textarea class="form-control" id="example-textarea"
+                                                        rows="5"></textarea>
                                                 </div>
-    
+
 
                                                 <div class="form-group">
                                                     <label for="simpleinput">Prefered Contact</label>
                                                     <input type="text" id="simpleinput" class="form-control">
                                                 </div>
 
-                                                
+
                                                 <div class="form-group">
                                                     <label for="simpleinput">Prefered Contact</label>
                                                     <input type="text" id="simpleinput" class="form-control">
                                                 </div>
 
-                                                
+
                                                 <div class="form-group">
                                                     <label for="simpleinput">Prefered Date</label>
                                                     <input type="date" id="simpleinput" class="form-control">
                                                 </div>
 
                                                 <div class="form-group text-center">
-                                                    <button class="btn btn-primary btn-rounded" type="button" id="next_button">
+                                                    <button class="btn btn-primary btn-rounded" type="button"
+                                                        id="next_button">
                                                         &nbsp; Submit &nbsp;</button>
                                                 </div>
 
                                             </form>
                                         </div> <!-- end col -->
-                                    </p>
-                               
-                                </div>
-                                <div class="tab-pane " id="profile-b2">
-                                   
-                                    <p>
+                                        </p>
+
+                                    </div>
+                                    <div class="tab-pane " id="profile-b2">
+
+                                        <p>
                                         <h4 class="text-primary text-center">Send us a complaint</h4>
                                         <hr>
-                                    </p>
+                                        </p>
 
-                                    <p>
+                                        <p>
                                         <div class="">
                                             <form>
 
@@ -119,57 +121,59 @@
 
                                                 <div class="form-group">
                                                     <label for="example-textarea">Message</label>
-                                                    <textarea class="form-control" id="example-textarea" rows="5"></textarea>
+                                                    <textarea class="form-control" id="example-textarea"
+                                                        rows="5"></textarea>
                                                 </div>
-    
+
 
                                                 <div class="form-group">
                                                     <label for="simpleinput">Prefered Contact</label>
                                                     <input type="text" id="simpleinput" class="form-control">
                                                 </div>
 
-                                                
+
                                                 <div class="form-group">
                                                     <label for="simpleinput">Prefered Contact</label>
                                                     <input type="text" id="simpleinput" class="form-control">
                                                 </div>
 
-                                                
+
                                                 <div class="form-group">
                                                     <label for="simpleinput">Prefered Date</label>
                                                     <input type="date" id="simpleinput" class="form-control">
                                                 </div>
 
                                                 <div class="form-group text-center">
-                                                    <button class="btn btn-primary btn-rounded" type="button" id="next_button">
+                                                    <button class="btn btn-primary btn-rounded" type="button"
+                                                        id="next_button">
                                                         &nbsp; Submit &nbsp;</button>
                                                 </div>
 
                                             </form>
                                         </div> <!-- end col -->
-                                    </p>
-                               
-                               
-                                </div>
-                               
-                            </div>
-                        </div> <!-- end card-box-->
-                    </div> <!-- end col -->
-               
-                    <div class="col-md-2">  </div>
-                </div>
-           
-         
-            </div>
+                                        </p>
 
-            <div class="col-md-2"></div>
+
+                                    </div>
+
+                                </div>
+                            </div> <!-- end card-box-->
+                        </div> <!-- end col -->
+
+                        <div class="col-md-2"> </div>
+                    </div>
+
+
+                </div>
+
+                <div class="col-md-2"></div>
+
+            </div>
 
         </div>
 
     </div>
 
-</div>
 
-  
 
 @endsection
