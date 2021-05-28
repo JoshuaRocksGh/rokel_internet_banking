87b7e367d8dfb9871c32c3aba2679549fd2df4d0
Joshua Tetteh <josh.tetteh01@gmail.com>
2021-05-28 17:06:21 +0000

    local bank final changes

--- resources/views/pages/transfer/beneficiary_list.blade.php
+++ resources/views/pages/transfer/beneficiary_list.blade.php
@@ -14,6 +14,31 @@
     <!-- third party css end -->
 
     <style>
+        @media print {
+            .hide_on_print {
+                display: none
+            }
+        }
+
+        @font-face {
+            font-family: 'password';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 40px;
+            src: url(https://jsbin-user-assets.s3.amazonaws.com/rafaelcastrocouto/password.ttf);
+        }
+
+        input.key {
+            font-family: 'password';
+            width: 300px;
+            height: 80px;
+            font-size: 100px;
+        }
+
+        .table_over_flow {
+            overflow-y: hidden;
+
+        }
 
     </style>
 
@@ -23,11 +48,41 @@
 
     <div>
 
+        <div class="container-fluid">
+            <br>
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-md-6">
+                    <h4 class="text-primary">
+                        <img src="{{ asset('assets/images/logoRKB.png') }}" alt="logo" style="zoom: 0.05">&emsp;
+                        TRANSFER BENEFICIARY LIST
+                    </h4>
+                </div>
+
+                <div class="col-md-6 text-right">
+                    <h6>
+
+                        <span class="flaot-right">
+                            <b class="text-primary"> Transfer </b> &nbsp; > &nbsp; <b class="text-danger">Transfer Beneficiary List</b>
+
+
+                        </span>
+
+                    </h6>
+
+                </div>
+
+                <div class="col-md-12 ">
+                    <hr class="text-primary" style="margin: 0px;">
+                </div>
+
+            </div>
+        </div>
 
         <div class="row">
             <br>
             <div class="col-12">
-                <div class="card">
+                <div class="">
 
                     <div class="card-body">
 
@@ -36,14 +91,8 @@
 
 
 
-                            <div class="col-md-1"></div>
-                            <div class="col-md-10">
-
-                                <h2 class="header-title m-t-0 text-primary">TRANSFER BENEFICIARY LIST</h2>
-                                <hr>
-
-
-
+                            {{--  <div class="col-md-1"></div>  --}}
+                            <div class="col-md-12">
 
                                 <div class="row">
                                     <div class="col-md-12">
@@ -76,14 +125,17 @@
 
                                     </div>
                                 </div>
-                                <div class="row" id="beneficiary_table" style="zoom: 0.8;">
-                                    <div class="col-md-12">
+                                <div class="row" id="beneficiary_table" >
+                                    <div class="col-md-12 col-sm-12 col-xs-12 m-2 customize_card" id="transaction_summary"
+                                    style="background-image: linear-gradient(to bottom right, white, rgb(201, 223, 230));">
+                                    <div class="p-3 mt-4 mt-lg-0 rounded">
+                                        <br>
 
                                         <table id="datatable-buttons"
                                             class="table table-bordered table-striped dt-responsive nowrap w-100 beneficiary_list_display">
                                             {{-- <table id="datatable-buttons" class="table table-bordered table-striped dt-responsive nowrap w-100"> --}}
                                             <thead>
-                                                <tr class="bg-secondary text-white">
+                                                <tr class="bg-primary text-white">
                                                     <th> <b> Alias </b> </th>
                                                     <th> <b> Account Number </b> </th>
                                                     <th> <b> Beneficiary Name </b> </th>
@@ -96,6 +148,10 @@
 
 
                                         </table>
+
+                                    </div>
+
+
                                     </div>
 
                                 </div>
@@ -234,7 +290,7 @@
 
                         </div>
 
-                        <div class="col-md-1"></div>
+                        {{--  <div class="col-md-1"></div>  --}}
 
 
 
