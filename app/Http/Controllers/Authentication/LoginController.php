a56ebe4744785c65056ee3212e0d6f69be553aeb
king kobby <ampahkwabena5@gmail.com>
2021-06-24 18:01:21 +0100

    .

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -94,7 +94,7 @@ class LoginController extends Controller
                         "lastLogin" => $user_detail->lastLogin,
                         "customerType" => $user_detail->customerType,
                         "checkerMaker" => $user_detail->checkerMaker,
-                        "checkerMaker" => 'M',
+                        "checkerMaker" => 'M', 
                         "headers"=>[
                         "x-api-key"=> "123",
                         "x-api-secret"=> "123",
